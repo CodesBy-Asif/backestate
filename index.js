3e56f69519256c40e455b961ba94a0be20bbe052
Manahil Afzal <manahil@example.com>
2025-09-04 10:41:48 -0700

    /

--- index.js
+++ index.js
@@ -25,8 +25,6 @@ app.use(cors({
     else callback(new Error("Not allowed by CORS"));
   },
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
 }));
 
 // Handle preflight requests globally
